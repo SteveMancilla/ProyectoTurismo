e8f8a7ad1761497f0d90131e587524aef0230830
steven <72620878@continental.edu.pe>
2024-09-21 15:42:04 -0500

    Se actualiza proyecto

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -5,8 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
+    @vite(['resources/css/app2.css'])
 </head>
 <body>
+    
+
+    <header>
+        <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur sed aliquid quasi molestiae voluptate est accusamus tenetur accusantium, iste inventore fugiat, sint nostrum illo ratione corporis error repellat sit blanditiis?</h1>
+    </header>
+
     @yield('content')
+
 </body>
 </html>
