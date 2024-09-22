078a0aa40d42c29815f51bd8db5c7d04267b052d
steven <72620878@continental.edu.pe>
2024-09-21 21:27:18 -0500

    Diseño de la pagina web

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
-    @vite(['resources/css/app2.css'])
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+
 </head>
 <body>
     
@@ -15,6 +16,7 @@
     </header>
 
     @yield('content')
+    <script src="{{ asset('js/script.js') }}"></script>
 
 </body>
 </html>
