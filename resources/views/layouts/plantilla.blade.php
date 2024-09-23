59819786dd0489ea415b2c2c0f2050b2fccbab2b
Nilton_Joel_Lazo_Maravi <74091904@continental.edu.pe>
2024-09-22 19:32:55 -0500

    Se actualizó el codigo

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -232,19 +232,7 @@
       
       <!-- end Tours -->
       <!-- Amazing -->
-      <div class="amazing">
-         <div class="container">
-            <div class="row">
-               <div class="col-md-12">
-                  <div class="amazing-box">
-                     <h2>Amazing London Tour</h2>
-                     <span>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there</span>
-                     <a href="#">Book Now</a><a href="#">Get More</a>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
+      
       <!-- end Amazing -->
       <!-- our blog -->
       <div id="blog" class="blog">
